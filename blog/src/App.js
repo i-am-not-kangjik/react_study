c36c50ed142512d5d1bb942d68d3d82d0c5fabb4
Kangjik Kim <rkqqkb@gmail.com>
2023-04-24 11:55:06 +0900

    state 사용법

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목, b] = useState(['남자 코트 추천', '여자 코트 추천', '맛집 추천'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>{ post }</h4>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
