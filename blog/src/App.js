2c7ce39c4da2f905118a0cc8403bc778a013ceb2
Kangjik Kim <rkqqkb@gmail.com>
2023-04-24 13:36:28 +0900

    map을 사용한 반복문

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   let blogTitle = 'ReactBlog';
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
   const handleSortClick = () => {
@@ -17,10 +17,10 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>{ blogTitle }</h4>
+        <h4>{blogTitle}</h4>
       </div>
 
-      <button onClick={()=> {
+      <button onClick={() => {
         let copy = [...글제목];
         copy[0] = '여자 코트 추천';
         글제목변경(copy);
@@ -28,7 +28,7 @@ function App() {
 
       <button onClick={handleSortClick}>제목 정렬</button>
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉++) } }>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
       </div>
@@ -39,7 +39,23 @@ function App() {
       <div className="list">
         <h4 onClick={()=>{ setModal(!modal) }}>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+      {
+        글제목.map(function (a, i) {
+          return (
+            <div className='list'>
+              <h4>{a}
+              <span onClick={() => {
+                let copy = [...따봉];
+                copy[i]++;
+                따봉변경(copy);
+              }}>👍</span>{따봉[i]}
+              </h4>
+              <p>2월 18일 발행</p>
+            </div>
+          )
+        })
+      }
 
       {
         modal == true ? <Modal></Modal> : null
@@ -49,13 +65,13 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal() {
   return (
     <div className='modal'>
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   )
 }
 
