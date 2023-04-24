5dd000606586a2677ae2fad5bc28109499459faf
Kangjik Kim <rkqqkb@gmail.com>
2023-04-24 12:20:55 +0900

    state 변경

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,16 +4,22 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코트 추천', '여자 코트 추천', '맛집 추천'])
+  let blogTitle = 'ReactBlog';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학'])
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>{ post }</h4>
+        <h4>{ blogTitle }</h4>
       </div>
+      <button onClick={()=> {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글수정</button>
       <div className="list">
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
