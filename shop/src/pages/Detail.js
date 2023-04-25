7fd44d352e48d7f57e669a244dd5374da79bc133
Kangjik Kim <rkqqkb@gmail.com>
2023-04-25 19:45:22 +0900

    고정 id 값을 참조하도록 변경

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -2,18 +2,19 @@ import { useParams } from "react-router-dom";
 
 function Detail(props) {
 
-    let {id} = useParams();
+    let { id } = useParams();
+    let found_id = props.info.find((x) => x.id == id)
 
     return (
         <div className="container mt-5">
             <div className="row">
-                <div className="col-md-6">
-                    <img src={process.env.PUBLIC_URL + '/mac' + (parseInt(id) + 1) + '.jpg'} className='img-fluid rounded' style={{maxHeight: '500px'}} />
+                <div className="col-md-6 mt-5">
+                    <img src={process.env.PUBLIC_URL + '/mac' + (parseInt(id) + 1) + '.jpg'} className='img-fluid rounded' style={{ maxHeight: '500px' }} />
                 </div>
-                <div className="col-md-6">
-                    <h2 className="pt-5">{props.info[id].title}</h2>
-                    <p style={{fontSize: '1.5rem'}}>{props.info[id].content}</p>
-                    <p style={{fontSize: '2rem', fontWeight: 'bold'}}>{props.info[id].price}</p>
+                <div className="col-md-6 mt-2">
+                    <h2 className="pt-5">{found_id.title}</h2>
+                    <p style={{ fontSize: '1.5rem' }}>{found_id.content}</p>
+                    <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>{found_id.price + '원'}</p>
                     <button className="btn btn-danger btn-lg">주문하기</button>
                 </div>
             </div>
