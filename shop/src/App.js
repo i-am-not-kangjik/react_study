0d40b3baee088c6d977bbee4c39ba1a9e5914108
Kangjik Kim <rkqqkb@gmail.com>
2023-04-25 17:32:03 +0900

    리액트 기본 라우팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import { Button, Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
@@ -15,32 +16,43 @@ function App() {
 
   return (
     <div className="App">
+
+
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">다판다</Navbar.Brand>
+          <Navbar.Brand href="/">다판다</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#cart">Cart</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/detail">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
-      <Container className='mt-4'>
-        {
-          chunks.map((chunk, rowIndex) => (
-            <Row key={rowIndex}>
-              {chunk.map((item, colIndex) => (
-                <Col key={colIndex} md={4}>
-                  <img src={process.env.PUBLIC_URL + '/mac' + (item.id+1) + '.jpg'} className='img-fluid' />
-                  <h4>{item.title}</h4>
-                  <p>{item.price}</p>
-                </Col>
-              ))}
-            </Row>
-          ))
+      <Routes>
+        <Route path='/' element={
+          <>
+            <div className='main-bg'></div>
+            <Container className='mt-4'>
+              {
+                chunks.map((chunk, rowIndex) => (
+                  <Row key={rowIndex}>
+                    {chunk.map((item, colIndex) => (
+                      <Col key={colIndex} md={4}>
+                        <img src={process.env.PUBLIC_URL + '/mac' + (item.id + 1) + '.jpg'} className='img-fluid' />
+                        <h4>{item.title}</h4>
+                        <p>{item.price}</p>
+                      </Col>
+                    ))}
+                  </Row>
+                ))
+              }
+            </Container>
+          </>
         }
-      </Container>
+        />
+        <Route path='/detail' element={<div>상세페이지임</div>} />
+      </Routes>
+
     </div>
   );
 }
