cccec8c30096ca6067e04ec983337162ed136b52
Kangjik Kim <rkqqkb@gmail.com>
2023-04-25 19:19:14 +0900

    url 파라미터로 상세페이지 만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -54,16 +54,16 @@ function App() {
           </>
         }
         />
-        <Route path='/detail' element={<Detail />} />
+        <Route path='/detail/:id' element={<Detail info={info} />} />
 
         <Route path='/about' element={<About />}>
           <Route path='member' element={<div>멤버임</div>} />
           <Route path='location' element={<div>위치임</div>} />
         </Route>
 
-        <Route path='/event' element={<EventPage/>}>
-          <Route path='one' element={<p>첫 주문시 양배추즙 서비스</p>}/>
-          <Route path='two' element={<p>생일기념 쿠폰받기</p>}/>
+        <Route path='/event' element={<EventPage />}>
+          <Route path='one' element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path='two' element={<p>생일기념 쿠폰받기</p>} />
 
         </Route>
 
@@ -84,7 +84,7 @@ function About() {
   )
 }
 
-function EventPage(){
+function EventPage() {
   return (
     <div>
       <h4>오늘의 이벤트</h4>
--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,15 +1,20 @@
-function Detail() {
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+
+    let {id} = useParams();
+
     return (
-        <div className="container">
+        <div className="container mt-5">
             <div className="row">
                 <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                    <img src={process.env.PUBLIC_URL + '/mac' + (parseInt(id) + 1) + '.jpg'} className='img-fluid rounded' style={{maxHeight: '500px'}} />
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
-                    <button className="btn btn-danger">주문하기</button>
+                    <h2 className="pt-5">{props.info[id].title}</h2>
+                    <p style={{fontSize: '1.5rem'}}>{props.info[id].content}</p>
+                    <p style={{fontSize: '2rem', fontWeight: 'bold'}}>{props.info[id].price}</p>
+                    <button className="btn btn-danger btn-lg">주문하기</button>
                 </div>
             </div>
         </div>
