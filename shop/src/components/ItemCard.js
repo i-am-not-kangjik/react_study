a9d525c9ab5ec5b6e947067fcd65d18faf1f137f
Kangjik Kim <rkqqkb@gmail.com>
2023-04-26 15:13:03 +0900

    이미지 id도 0부터 시작하게 수정

--- shop/src/components/ItemCard.js
+++ shop/src/components/ItemCard.js
@@ -4,7 +4,7 @@ function ItemCard({ item, onItemClick }) {
   return (
     <Col md={4}>
       <Card onClick={() => onItemClick(item.id)}>
-        <Card.Img variant="top" src={process.env.PUBLIC_URL + '/laptop' + (item.id + 1) + '.jpg'} className='img-fluid' />
+        <Card.Img variant="top" src={process.env.PUBLIC_URL + '/laptop' + (item.id) + '.jpg'} className='img-fluid' />
         <Card.Body>
           <Card.Title>{item.title}</Card.Title>
           <Card.Text>{item.price}</Card.Text>
